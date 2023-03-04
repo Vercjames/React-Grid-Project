ef7c9e0d69dbd5279e33c6238b026af5a94affd2
Verc James <vercjames@gmail.com>
2023-03-04 12:31:38 -0600

    Grid System Demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,85 @@
+import React from 'react';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {Box, Card, Grid, Typography} from "@mui/material";
 
-function App() {
+
+export const App = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Box className="App">
+      <section style={{ margin: 10}}>
+        <Typography variant="h6" component="h6">
+          Fixed Grid Showcasing Column and Row Spacing
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Card sx={{ padding: 1, backgroundColor: "black"}}>
+              <Typography variant="body1" component="p" sx={{ textAlign: "center", color: "white"}}>
+                8 Units
+              </Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <Card sx={{ padding: 1, backgroundColor: "black"}}>
+              <Typography variant="body1" component="p" sx={{ textAlign: "center", color: "white"}}>
+                4 Units
+              </Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <Card sx={{ padding: 1, backgroundColor: "black"}}>
+              <Typography variant="body1" component="p" sx={{ textAlign: "center", color: "white"}}>
+                4 Units
+              </Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={8}>
+            <Card sx={{ padding: 1, backgroundColor: "black"}}>
+              <Typography variant="body1" component="p" sx={{ textAlign: "center", color: "white"}}>
+                8 Units
+              </Typography>
+            </Card>
+          </Grid>
+        </Grid>
+      </section>
+
+      <section style={{ margin: 10}}>
+        <Typography variant="h6" component="h6">
+          Reactive Grid that is mobile responsive
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={3}>
+            <Card sx={{ padding: 1, backgroundColor: "black"}}>
+              <Typography variant="body1" component="p" sx={{ textAlign: "center", color: "white"}}>
+                3md, 6sm, 12xs
+              </Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Card sx={{ padding: 1, backgroundColor: "black"}}>
+              <Typography variant="body1" component="p" sx={{ textAlign: "center", color: "white"}}>
+                3md, 6sm, 12xs
+              </Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Card sx={{ padding: 1, backgroundColor: "black"}}>
+              <Typography variant="body1" component="p" sx={{ textAlign: "center", color: "white"}}>
+               3md, 6sm, 12xs
+              </Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Card sx={{ padding: 1, backgroundColor: "black"}}>
+              <Typography variant="body1" component="p" sx={{ textAlign: "center", color: "white"}}>
+                3md, 6sm, 12xs
+              </Typography>
+            </Card>
+          </Grid>
+        </Grid>
+      </section>
+
+    </Box>
   )
 }
-
-export default App
